394f358cbe99c669fdbd3f675c350880b48caa5e
Nicholas Pereira <nicholastimotheoff@gmail.com>
2026-08-08 10:32:47 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/vestingCustodyApp/vca/VeriMediaApplication.java
@@ -0,0 +1,13 @@
+package com.vestingCustodyApp.vca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeriMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeriMediaApplication.class, args);
+	}
+
+}

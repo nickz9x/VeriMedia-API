6e85d5aacc44e16010b36f0fa20e965df25493bf
Nicholas Pereira <nicholastimotheoff@gmail.com>
2026-08-15 22:13:14 -0300

    chore: externalize secrets, add springdoc and clean up project
    - JWT secret now required via JWT_SECRET env var (rotated); .env.example added
    - springdoc-openapi 3.0.3 (Swagger UI at /swagger-ui)
    - remove unused OAuth2 dependencies and dead HashMedia class
    - .gitattributes for cross-platform line endings

--- src/main/java/com/vestingCustodyApp/vca/service/TokenService.java
+++ src/main/java/com/vestingCustodyApp/vca/service/TokenService.java
@@ -6,7 +6,6 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.vestingCustodyApp.vca.entity.User;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
--- src/main/java/com/vestingCustodyApp/vca/util/HashMedia.java
+++
@@ -1,9 +0,0 @@
-package com.vestingCustodyApp.vca.util;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class HashMedia {
-
-
-}
